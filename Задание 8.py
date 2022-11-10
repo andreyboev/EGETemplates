27793f420a6656cd36aa9bcb81767d8dffc54cc7
Андрей Боев <a.boev.it@gmail.com>
2022-11-10 19:08:18 +0300

    Added comment to task 24

--- Задание 8.py
+++ Задание 8.py
@@ -8,11 +8,14 @@
 '''
 
 import itertools
-
+# itertools - библиотека для работы с комбинаторикой
+# product() - функция, получающая все возможные перестановки элементов длины repeat из букв, которые в неё переданы
 listString = itertools.product('БОРИС', repeat=6)
 count = 0
 for str in listString:
+    # join() - функция соединяющая массив строк в одну строку при помощи разделителя, который указан до точки
     line = ''.join(str)
+    # count() - строковая функция, которая определяет кол-во вхождений букв или слов в строку
     if line.count('Б') == 1 and line.count('Р') == 1 and line.count('С') < 2:
         count += 1
 print(count)

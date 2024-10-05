df029fdbd634e1d620a4b0502d46101f5913db59
Андрей Боев <a.boev.it@gmail.com>
2024-10-05 13:45:36 +0300

    Изменение логики вывода результата

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,86 +1,118 @@
 import os
+import re
 from datetime import datetime
 
 import pytest
 import sqlite3
 
-from matplotlib import pyplot as plt
+from matplotlib import pyplot as plt, patches
 from matplotlib.colors import Normalize, LinearSegmentedColormap
+from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
 
 
 def create_new_db():
-    connection = sqlite3.connect('result.db')
-    cursor = connection.cursor()
-    cursor.execute('''
-    CREATE TABLE IF NOT EXISTS test (
-        date_time   DATETIME,
-        task_number BIGINT,
-        task_type   INTEGER,
-        result      INTEGER
-    )
-    ''')
-    connection.commit()
-    connection.close()
+    with sqlite3.connect('result.db') as connection:
+        cursor = connection.cursor()
+        cursor.execute('''
+        CREATE TABLE IF NOT EXISTS test (
+            date_time   DATETIME,
+            task_number BIGINT,
+            task_type   INTEGER,
+            result      INTEGER
+        )
+        ''')
 
 def add_result(date_time, task_number, task_type, result):
-    connection = sqlite3.connect('result.db')
-    cursor = connection.cursor()
-    cursor.execute('insert into test (date_time, task_number, task_type, result) values (?, ?, ?, ?)',
-                   (date_time, task_number, task_type, result))
-    connection.commit()
-    connection.close()
+    with sqlite3.connect('result.db') as connection:
+        cursor = connection.cursor()
+        cursor.execute('INSERT INTO test (date_time, task_number, task_type, result) VALUES (?, ?, ?, ?)',
+                       (date_time, task_number, task_type, result))
 
 def update_result(date_time, task_number, task_type, result):
-    connection = sqlite3.connect('result.db')
-    cursor = connection.cursor()
-    cursor.execute('update test set date_time = ?, result = ? where task_type = ? and task_number = ?',
-                   (date_time, result, task_type, task_number))
-    connection.commit()
-    connection.close()
+    with sqlite3.connect('result.db') as connection:
+        cursor = connection.cursor()
+        cursor.execute('UPDATE test SET date_time = ?, result = ? WHERE task_type = ? AND task_number = ?',
+                       (date_time, result, task_type, task_number))
 
 def get_result(task_number, task_type):
-    connection = sqlite3.connect('result.db')
-    cursor = connection.cursor()
-    cursor.execute('select * from test where task_number = ? and task_type = ?',
-                   (task_number, task_type))
-    result = cursor.fetchone()
-    connection.commit()
-    connection.close()
-    return result
+    with sqlite3.connect('result.db') as connection:
+        cursor = connection.cursor()
+        cursor.execute('SELECT * FROM test WHERE task_number = ? AND task_type = ?',
+                       (task_number, task_type))
+        return cursor.fetchone()
 
 def get_results():
-    connection = sqlite3.connect('result.db')
-    cursor = connection.cursor()
-    cursor.execute('select * from test')
-    result = cursor.fetchall()
-    connection.commit()
-    connection.close()
-    return result
+    with sqlite3.connect('result.db') as connection:
+        cursor = connection.cursor()
+        cursor.execute('SELECT * FROM test')
+        return cursor.fetchall()
 
-def clamp(n, smallest, largest): return max(smallest, min(n, largest))
+def clamp(n, smallest, largest):
+    return max(smallest, min(n, largest))
 
 def show_available_task(tasks: list):
     square_size = 1   # Размер квадрата
     spacing = 0.2     # Промежуток между квадратами
     columns = 10      # Количество значений в строке
-
     fig, ax = plt.subplots()
-
     for i, value in enumerate(tasks):
         x = (i % columns) * (square_size + spacing)
         y = -(i // columns) * (square_size + spacing)
-
         square = plt.Rectangle((x, y), square_size, square_size, facecolor='blue', edgecolor='black')
         ax.add_patch(square)
-
         ax.text(x + square_size / 2, y + square_size / 2, str(value), ha='center', va='center', color='white')
-
     ax.set_xlim(0, columns * (square_size + spacing) - spacing)
     ax.set_ylim(-((len(tasks) // columns) + 1) * (square_size + spacing), square_size + 0.5)
-
+    ax.set_aspect('equal')
     ax.axis('off')
-    plt.title('Доступные для решения задания')
-    plt.show()
+    ax.set_title('Задания, доступные для исправления')
+    return fig
+
+def show_task_results(current_passed, current_failed):
+    fig, ax = plt.subplots(figsize=(10, 3))
+    for i, (task_type, task_number) in enumerate(current_passed):
+        rect = patches.Rectangle((i, 1), 1, 1, linewidth=1, edgecolor='green', facecolor='lightgreen')
+        ax.add_patch(rect)
+        ax.text(i + 0.5, 1.5, f'{task_type}-{task_number}', ha='center', va='center', color='black')
+    for i, (task_type, task_number) in enumerate(current_failed):
+        rect = patches.Rectangle((i, 0), 1, 1, linewidth=1, edgecolor='red', facecolor='lightcoral')
+        ax.add_patch(rect)
+        ax.text(i + 0.5, 0.5, f'{task_type}-{task_number}', ha='center', va='center', color='black')
+    ax.set_xlim(0, max(len(current_passed), len(current_failed)))
+    ax.set_ylim(-0.5, 2)
+    ax.set_xticks([])
+    ax.set_yticks([0.5, 1.5])
+    ax.set_yticklabels(['Неправильно', 'Правильно'])
+    ax.set_title('Результаты текущей сессии')
+    return fig
+
+def plot_preparation_level(types):
+    t = [i + 1 for i in range(27)]
+    values = [item[4] for item in types]
+    norm = Normalize(vmin=0, vmax=100)
+    cmap = LinearSegmentedColormap.from_list("red_green", ["red", "orange", "green"])
+    colors = cmap(norm(values))
+    fig, ax = plt.subplots(figsize=(12, 5))
+    bars = ax.bar(t, values, width=0.8, color=colors)
+    for bar in bars:
+        yval = bar.get_height()
+        ax.text(bar.get_x() + bar.get_width() / 2, yval / 2, round(yval, 2),
+                ha='center', va='center')
+    ax.set_xlabel('Тип задания')
+    ax.set_ylabel('Значение')
+    ax.set_title('Уровень подготовки')
+    ax.set_xticks(t)
+    return fig
+
+def extract_test_info(test_string):
+    pattern = r'test_scripts\[(\d+)-[a-f0-9]+-TestType_(\d+)\]'
+    match = re.search(pattern, test_string)
+    if match:
+        test_number = match.group(1)
+        test_type = match.group(2)
+        return int(test_type), int(test_number)
+    else:
+        return None, None
 
 @pytest.hookimpl()
 def pytest_sessionstart(session):
@@ -94,60 +126,75 @@ def pytest_sessionstart(session):
 def pytest_runtest_makereport(item, call):
     outcome = yield
     result = outcome.get_result()
-
     if result.when == 'call':
         item.session.results[item] = result
 
 @pytest.hookimpl()
 def pytest_sessionfinish(session, exitstatus):
-    for result in session.results.values():
-        _, type, task = result.nodeid.split('::')
-        type = int(type.split('_')[1])
-        task = int(task.split('_')[1])
+    current_passed = []
+    current_failed = []
+    now = datetime.now()
+    for result in filter(lambda r: r.outcome != 'skipped', session.results.values()):
+        type, task = extract_test_info(result.nodeid)
         old_solution = None
         if (type, task) in session.old_results:
             old_solution = get_result(task, type)
+        # Если ранее данное задание не решалось или пришло время перерешивания
         if (session.old_results.count((type, task)) == 0 or
                 (old_solution is not None and abs((datetime.fromtimestamp(old_solution[0]) - datetime.now()).days) >= 14)):
+            # Если решение есть в БД
             if old_solution is not None:
-                update_result(datetime.now().timestamp(), task, type, 1 if result.passed else 0)
+                update_result(now.timestamp(), task, type, 1 if result.passed else 0)
             else:
-                if not result.passed and os.path.exists(f'../Тема {type}/Задания/Задание {task}.py'):
-                    os.rename(f'../Тема {type}/Задания/Задание {task}.py', f'../Тема {type}/Задания/-Задание {task}.py')
-                add_result(datetime.now().timestamp(), task, type, 1 if result.passed else 0)
+                task_path = f'../Тема {type}/Задания/Задание {task}.py'
+                if not result.passed and os.path.exists(task_path):
+                    os.rename(task_path, f'../Тема {type}/Задания/-Задание {task}.py')
+                add_result(now.timestamp(), task, type, 1 if result.passed else 0)
+            if result.passed:
+                current_passed.append((type, task))
+            else:
+                current_failed.append((type, task))
     results = get_results()
     # тип, правильно, неправильно, всего, результат
     #  0       1           2         3        4
     types = [[i + 1, 0, 0, 0, 0] for i in range(27)]
     for item in session.items:
-        _, type, task = item.nodeid.split('::')
-        type = int(type.split('_')[1])
+        type, task = extract_test_info(item.nodeid)
         types[type - 1][3] += 1
     # date_time, task_number, task_type, result
     available_task = []
     for res in results:
-        if abs((datetime.fromtimestamp(res[0]) - datetime.now()).days) >= 14:
+        if abs((datetime.fromtimestamp(res[0]) - now).days) >= 14:
             available_task.append(f'{res[2]}.{res[1]}')
         types[res[2] - 1][1] += 1 if res[3] == 1 else 0
         types[res[2] - 1][2] += 1 if res[3] == 0 else 0
 
-    if available_task:
-        show_available_task(available_task)
+    fig, (ax1, ax2, ax3) = plt.subplots(3, 1, figsize=(12, 16))
+
+    available_task_fig = show_available_task(available_task)
+    available_task_canvas = FigureCanvas(available_task_fig)
+    available_task_canvas.draw()
+    available_task_rgba_buffer = available_task_canvas.buffer_rgba()
+    ax3.imshow(available_task_rgba_buffer, aspect='equal')
+    ax3.axis('off')
 
     for t in types:
         if t[3] > 0:
             t[4] = clamp((100 - ((t[2] + t[3] - t[1]) / t[3]) * 100), 0, 100)
 
-    t = [i + 1 for i in range(27)]
-    values = [item[4] for item in types]
+    task_results_fig = show_task_results(current_passed, current_failed)
+    task_results_canvas = FigureCanvas(task_results_fig)
+    task_results_canvas.draw()
+    task_results_rgba_buffer = task_results_canvas.buffer_rgba()
+    ax1.imshow(task_results_rgba_buffer, aspect='equal')
+    ax1.axis('off')
 
-    norm = Normalize(vmin=0, vmax=100)
-    cmap = LinearSegmentedColormap.from_list("red_green", ["red", "orange", "green"])
-    colors = cmap(norm(values))
+    preparation_level_fig = plot_preparation_level(types)
+    preparation_level_canvas = FigureCanvas(preparation_level_fig)
+    preparation_level_canvas.draw()
+    preparation_level_rgba_buffer = preparation_level_canvas.buffer_rgba()
+    ax2.imshow(preparation_level_rgba_buffer, aspect='equal')
+    ax2.axis('off')
 
-    plt.bar(t, values, width=0.8, color=colors)
-    plt.xlabel('Тип задания')
-    plt.ylabel('Значение')
-    plt.title('Уровень подготовки')
-    plt.xticks(t)
-    plt.show()
+    fig.tight_layout()
+    fig.show()
--- tests/test.py
+++ tests/test.py
@@ -1,148 +1,39 @@
 import hashlib
-import logging
 import os
 import subprocess
 import pytest
 
-file_paths = ['..\\Тема 1\\Задания\\Задание 1.py',
-              '..\\Тема 2\\Задания\\Задание 2.py', '..\\Тема 3\\Задания\\Задание 3.py', '..\\Тема 4\\Задания\\Задание 4.py',
-              '..\\Тема 5\\Задания\\Задание 5.py', '..\\Тема 6\\Задания\\Задание 6.py', '..\\Тема 7\\Задания\\Задание 7.py',
-              '..\\Тема 8\\Задания\\Задание 8.py', '..\\Тема 9\\Задания\\Задание 9.py', '..\\Тема 1\\Задания\\Задание 10.py',
-              '..\\Тема 2\\Задания\\Задание 11.py', '..\\Тема 3\\Задания\\Задание 12.py', '..\\Тема 4\\Задания\\Задание 13.py',
-              '..\\Тема 5\\Задания\\Задание 14.py', '..\\Тема 6\\Задания\\Задание 15.py', '..\\Тема 7\\Задания\\Задание 16.py',
-              '..\\Тема 8\\Задания\\Задание 17.py', '..\\Тема 9\\Задания\\Задание 18.py']
-class TestType_1:
-    @pytest.mark.skipif(not os.path.exists(file_paths[0]), reason="Ещё не решено")
-    def test_1(self):
-        result = subprocess.run(['python', file_paths[0]], text=True, stdout=subprocess.PIPE,
-                                stderr=subprocess.PIPE)
-        hash = hashlib.md5(result.stdout.strip().encode()).hexdigest()
-        assert hash == '735b90b4568125ed6c3f678819b6e058', "Script failed to run successfully"
+def run_script_and_check(file_path, expected_hash, test_type):
+    result = subprocess.run(['python', file_path], text=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, encoding='utf-8')
+    output_hash = hashlib.md5(result.stdout.strip().encode()).hexdigest()
+    assert output_hash == expected_hash, f"{test_type}: Script failed to run successfully"
 
-    @pytest.mark.skipif(not os.path.exists(file_paths[9]), reason="Ещё не решено")
-    def test_10(self):
-        result = subprocess.run(['python', file_paths[9]], text=True, stdout=subprocess.PIPE,
-                                stderr=subprocess.PIPE)
-        hash = hashlib.md5(result.stdout.strip().encode()).hexdigest()
-        assert hash == 'aab3238922bcc25a6f606eb525ffdc56', "Script failed to run successfully"
+class TestScripts:
+    test_cases = [
+        (1, '735b90b4568125ed6c3f678819b6e058', 'TestType_1'),
+        (10, 'aab3238922bcc25a6f606eb525ffdc56', 'TestType_1'),
+        (2, 'e0abee87e4ba1de22c6b8cf076c5016b', 'TestType_2'),
+        (11, '7379de4777f5748aa568b8d0bf8c3795', 'TestType_2'),
+        (3, '25f44e52e16f79a3cacc8fdd3c3abf29', 'TestType_3'),
+        (12, 'c3a268e16fb2c7ae9f2c1ef90d42008f', 'TestType_3'),
+        (4, 'c74d97b01eae257e44aa9d5bade97baf', 'TestType_4'),
+        (13, '698d51a19d8a121ce581499d7b701668', 'TestType_4'),
+        (5, '7f975a56c761db6506eca0b37ce6ec87', 'TestType_5'),
+        (14, '1f0e3dad99908345f7439f8ffabdffc4', 'TestType_5'),
+        (6, 'eecca5b6365d9607ee5a9d336962c534', 'TestType_6'),
+        (15, '093f65e080a295f8076b1c5722a46aa2', 'TestType_6'),
+        (7, 'ea5d2f1c4608232e07d3aa3d998e5135', 'TestType_7'),
+        (16, 'e4da3b7fbbce2345d7772b0674a318d5', 'TestType_7'),
+        (8, '5249ee8e0cff02ad6b4cc0ee0e50b7d1', 'TestType_8'),
+        (17, 'd67d496249f30f93dd6a7a6d84701d60', 'TestType_8'),
+        (9, '812b4ba287f5ee0bc9d43bbf5bbe87fb', 'TestType_9'),
+        (18, '67388f1834f7d6243b753ec33584a8df', 'TestType_9')
+    ]
 
-class TestType_2:
-    @pytest.mark.skipif(not os.path.exists(file_paths[1]), reason="Ещё не решено")
-    def test_2(self):
-        result = subprocess.run(['python', file_paths[1]], text=True, stdout=subprocess.PIPE,
-                                stderr=subprocess.PIPE)
-        hash = hashlib.md5(result.stdout.strip().encode()).hexdigest()
-        assert hash == 'e0abee87e4ba1de22c6b8cf076c5016b', "Script failed to run successfully"
-
-    @pytest.mark.skipif(not os.path.exists(file_paths[10]), reason="Ещё не решено")
-    def test_11(self):
-        result = subprocess.run(['python', file_paths[10]], text=True, stdout=subprocess.PIPE,
-                                stderr=subprocess.PIPE)
-        hash = hashlib.md5(result.stdout.strip().encode()).hexdigest()
-        assert hash == '7379de4777f5748aa568b8d0bf8c3795', "Script failed to run successfully"
-
-class TestType_3:
-    @pytest.mark.skipif(not os.path.exists(file_paths[2]), reason="Ещё не решено")
-    def test_3(self):
-        result = subprocess.run(['python', file_paths[2]], text=True, stdout=subprocess.PIPE,
-                                stderr=subprocess.PIPE)
-        hash = hashlib.md5(result.stdout.strip().encode()).hexdigest()
-        assert hash == '25f44e52e16f79a3cacc8fdd3c3abf29', "Script failed to run successfully"
-
-    @pytest.mark.skipif(not os.path.exists(file_paths[11]), reason="Ещё не решено")
-    def test_12(self):
-        result = subprocess.run(['python', file_paths[11]], text=True, stdout=subprocess.PIPE,
-                                stderr=subprocess.PIPE, encoding='utf-8')
-        hash = hashlib.md5(result.stdout.strip().encode()).hexdigest()
-        assert hash == 'c3a268e16fb2c7ae9f2c1ef90d42008f', "Script failed to run successfully"
-
-
-class TestType_4:
-    @pytest.mark.skipif(not os.path.exists(file_paths[3]), reason="Ещё не решено")
-    def test_4(self):
-        result = subprocess.run(['python', file_paths[3]], text=True, stdout=subprocess.PIPE,
-                                stderr=subprocess.PIPE)
-        hash = hashlib.md5(result.stdout.strip().encode()).hexdigest()
-        assert hash == 'c74d97b01eae257e44aa9d5bade97baf', "Script failed to run successfully"
-
-    @pytest.mark.skipif(not os.path.exists(file_paths[12]), reason="Ещё не решено")
-    def test_13(self):
-        result = subprocess.run(['python', file_paths[12]], text=True, stdout=subprocess.PIPE,
-                                stderr=subprocess.PIPE)
-        hash = hashlib.md5(result.stdout.strip().encode()).hexdigest()
-        assert hash == '698d51a19d8a121ce581499d7b701668', "Script failed to run successfully"
-
-class TestType_5:
-    @pytest.mark.skipif(not os.path.exists(file_paths[4]), reason="Ещё не решено")
-    def test_5(self):
-        result = subprocess.run(['python', file_paths[4]], text=True, stdout=subprocess.PIPE,
-                                stderr=subprocess.PIPE)
-        hash = hashlib.md5(result.stdout.strip().encode()).hexdigest()
-        assert hash == '7f975a56c761db6506eca0b37ce6ec87', "Script failed to run successfully"
-
-    @pytest.mark.skipif(not os.path.exists(file_paths[13]), reason="Ещё не решено")
-    def test_14(self):
-        result = subprocess.run(['python', file_paths[13]], text=True, stdout=subprocess.PIPE,
-                                stderr=subprocess.PIPE)
-        hash = hashlib.md5(result.stdout.strip().encode()).hexdigest()
-        assert hash == '1f0e3dad99908345f7439f8ffabdffc4', "Script failed to run successfully"
-
-class TestType_6:
-    @pytest.mark.skipif(not os.path.exists(file_paths[5]), reason="Ещё не решено")
-    def test_6(self):
-        result = subprocess.run(['python', file_paths[5]], text=True, stdout=subprocess.PIPE,
-                                stderr=subprocess.PIPE)
-        hash = hashlib.md5(result.stdout.strip().encode()).hexdigest()
-        assert hash == 'eecca5b6365d9607ee5a9d336962c534', "Script failed to run successfully"
-
-    @pytest.mark.skipif(not os.path.exists(file_paths[14]), reason="Ещё не решено")
-    def test_15(self):
-        result = subprocess.run(['python', file_paths[14]], text=True, stdout=subprocess.PIPE,
-                                stderr=subprocess.PIPE)
-        hash = hashlib.md5(result.stdout.strip().encode()).hexdigest()
-        assert hash == '093f65e080a295f8076b1c5722a46aa2', "Script failed to run successfully"
-
-class TestType_7:
-    @pytest.mark.skipif(not os.path.exists(file_paths[6]), reason="Ещё не решено")
-    def test_7(self):
-        result = subprocess.run(['python', file_paths[6]], text=True, stdout=subprocess.PIPE,
-                                stderr=subprocess.PIPE)
-        hash = hashlib.md5(result.stdout.strip().encode()).hexdigest()
-        assert hash == 'ea5d2f1c4608232e07d3aa3d998e5135', "Script failed to run successfully"
-
-    @pytest.mark.skipif(not os.path.exists(file_paths[15]), reason="Ещё не решено")
-    def test_16(self):
-        result = subprocess.run(['python', file_paths[15]], text=True, stdout=subprocess.PIPE,
-                                stderr=subprocess.PIPE)
-        hash = hashlib.md5(result.stdout.strip().encode()).hexdigest()
-        assert hash == 'e4da3b7fbbce2345d7772b0674a318d5', "Script failed to run successfully"
-
-class TestType_8:
-    @pytest.mark.skipif(not os.path.exists(file_paths[7]), reason="Ещё не решено")
-    def test_8(self):
-        result = subprocess.run(['python', file_paths[7]], text=True, stdout=subprocess.PIPE,
-                                stderr=subprocess.PIPE)
-        hash = hashlib.md5(result.stdout.strip().encode()).hexdigest()
-        assert hash == '5249ee8e0cff02ad6b4cc0ee0e50b7d1', "Script failed to run successfully"
-
-    @pytest.mark.skipif(not os.path.exists(file_paths[16]), reason="Ещё не решено")
-    def test_17(self):
-        result = subprocess.run(['python', file_paths[16]], text=True, stdout=subprocess.PIPE,
-                                stderr=subprocess.PIPE)
-        hash = hashlib.md5(result.stdout.strip().encode()).hexdigest()
-        assert hash == 'd67d496249f30f93dd6a7a6d84701d60', "Script failed to run successfully"
-
-class TestType_9:
-    @pytest.mark.skipif(not os.path.exists(file_paths[8]), reason="Ещё не решено")
-    def test_9(self):
-        result = subprocess.run(['python', file_paths[8]], text=True, stdout=subprocess.PIPE,
-                                stderr=subprocess.PIPE)
-        hash = hashlib.md5(result.stdout.strip().encode()).hexdigest()
-        assert hash == '812b4ba287f5ee0bc9d43bbf5bbe87fb', "Script failed to run successfully"
-
-    @pytest.mark.skipif(not os.path.exists(file_paths[17]), reason="Ещё не решено")
-    def test_18(self):
-        result = subprocess.run(['python', file_paths[17]], text=True, stdout=subprocess.PIPE,
-                                stderr=subprocess.PIPE)
-        hash = hashlib.md5(result.stdout.strip().encode()).hexdigest()
-        assert hash == '67388f1834f7d6243b753ec33584a8df', "Script failed to run successfully"
+    @pytest.mark.parametrize("file_index, expected_hash, test_type", test_cases)
+    def test_scripts(self, file_index, expected_hash, test_type):
+        file_path = f'../Тема {int(test_type.split("_")[1])}/Задания/Задание {file_index}.py'
+        if not os.path.exists(file_path):
+            pytest.skip(reason="Ещё не решено")
+        else:
+            run_script_and_check(file_path, expected_hash, test_type)
